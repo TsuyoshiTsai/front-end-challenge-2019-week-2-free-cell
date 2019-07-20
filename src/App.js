957dc76c143afc1492f0c1a1008a9667595ffa13
Patrick <patrick3948@gmail.com>
2019-07-20 23:54:52 +0800

    Update webpack.config for deleting react-loadable/babel and styles

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react'
 import { hot } from 'react-hot-loader'
 import { withRouter } from 'react-router-dom'
 // import PropTypes from 'prop-types'
-// import classnames from 'classnames/bind'
+import classnames from 'classnames/bind'
 
 // Style
-// import styles from './style.module.scss'
+import styles from './style.module.scss'
+
+// Variables / Functions
+const cx = classnames.bind(styles)
 
 export const propTypes = {}
 
 function App (props) {
-  return <div>app</div>
+  return <div className={cx('app')}>app</div>
 }
 
 App.propTypes = propTypes
