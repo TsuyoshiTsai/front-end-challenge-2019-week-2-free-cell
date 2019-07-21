1b13f288a7b2afe43a8e12b7f0f3a9519997e6f2
Patrick <patrick3948@gmail.com>
2019-07-22 01:23:09 +0800

    Update content and layout

--- src/js/components/Content/index.js
+++ src/js/components/Content/index.js
@@ -14,10 +14,40 @@ export const propTypes = {}
 
 function Content (props) {
   // const a = new Card({ number: 1, suit: CardSuit.heart })
-  const a = new Game()
-  console.log('a :', a)
+  const game = new Game()
 
-  return <div className={cx('content')}>Content</div>
+  return (
+    <div className={cx('content')}>
+      <div className={cx('stack-container')}>
+        {new Array(4).fill(0).map((empty, index) => (
+          <div key={index} className={cx('parking')}>
+            {index}
+          </div>
+        ))}
+
+        <div>king</div>
+
+        {new Array(4).fill(0).map((empty, index) => (
+          <div key={index} className={cx('fundation')}>
+            {index}
+          </div>
+        ))}
+      </div>
+
+      <div className={cx('pile-list')}>
+        {game.piles.map((pile, index) => (
+          <div key={index} className={cx('pile-item')}>
+            <div className={cx('parking')}>parking</div>
+            {pile.cards.map((card, index) => (
+              <div key={index} className={cx('card')} style={{ top: index * 50 }}>
+                {card.rank} {card.suit}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 Content.propTypes = propTypes
