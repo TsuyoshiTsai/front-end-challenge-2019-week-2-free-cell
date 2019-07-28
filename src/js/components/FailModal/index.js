3b0a3d6ef7a903ebbc1f8883f5fac3744b7e2f69
Patrick <patrick3948@gmail.com>
2019-07-29 00:09:00 +0800

    Update FailModal

--- src/js/components/FailModal/index.js
+++ src/js/components/FailModal/index.js
@@ -19,20 +19,20 @@ function FailModal (props) {
   return (
     <Modal {...restProps}>
       <Modal.Header>
-        <Brand.Image type='angry' />
+        <Brand.Image type='sad' />
         <Typography.Text size='xl' align='center' lineHeight={1.25} marginTop={15} isBlock>
-          NO MORE MOVE
+          NO MORE MOVES
         </Typography.Text>
       </Modal.Header>
       <Modal.Body>
-        <Typography.Text letterSpacing='0.05em'>Are you sure you want to give up?</Typography.Text>
+        <Typography.Text letterSpacing='0.05em'>There are no moves available.</Typography.Text>
       </Modal.Body>
       <Modal.Footer>
-        <Button type='primary' size='sm' shape='rounded' width={150} onClick={onGiveUp}>
-          NEW GAME
-        </Button>
         <Button type='primary' size='sm' shape='rounded' width={150} onClick={onRestart}>
           RESTART
+        </Button>
+        <Button type='primary' size='sm' shape='rounded' width={150} onClick={onGiveUp}>
+          NEW GAME
         </Button>
         <Button type='primary' size='sm' shape='rounded' width={150} onClick={onUndo}>
           UNDO
